d82a84917e75c114f010e35edd11f996883691ea
thewayiam <twly.tw@gmail.com>
2013-12-28 11:30:52 +0800

    issue1 release to production done

--- bill/crawler.py
+++ bill/crawler.py
@@ -10,7 +10,6 @@ def write_file(data, file_name):
     file.write(data)
     file.close()
 
-#r = requests.get('http://api.ly.g0v.tw/v0/collections/bills/')
 bill_count = {'l': requests.get('http://api.ly.g0v.tw/v0/collections/bills/').json()['paging']['count']}
 response = requests.get('http://api.ly.g0v.tw/v0/collections/bills/', params=bill_count)
 objs = json.loads(response.text)
@@ -18,3 +17,11 @@ dump_data = json.dumps(objs)
 write_file(dump_data, 'lyapi_bills.json')
 dump_data = json.dumps(objs, sort_keys=True, indent=4, ensure_ascii=False)
 write_file(dump_data, 'lyapi_bills(pretty_format).json')
+
+bill_count = {'l': requests.get('http://api.ly.g0v.tw/v0/collections/ttsmotions/').json()['paging']['count']}
+response = requests.get('http://api.ly.g0v.tw/v0/collections/ttsmotions/', params=bill_count)
+objs = json.loads(response.text)
+dump_data = json.dumps(objs)
+write_file(dump_data, 'lyapi_ttsmotions.json')
+dump_data = json.dumps(objs, sort_keys=True, indent=4, ensure_ascii=False)
+write_file(dump_data, 'lyapi_ttsmotions(pretty_format).json')
