56e6c3e24dd0187b7dbc5f8cd8b7615ca15c4c63
thewayiam <twly.tw@gmail.com>
2014-03-15 17:47:13 +0800

    weekly data update, bills crawler try to use l&sk to avoid JSON truncate but still fail

--- bill/crawler.py
+++ bill/crawler.py
@@ -10,17 +10,27 @@ def write_file(data, file_name):
     file.write(data)
     file.close()
 
-bill_count = {'l': requests.get('http://api.ly.g0v.tw/v0/collections/bills/').json()['paging']['count']}
-response = requests.get('http://api.ly.g0v.tw/v0/collections/bills/', params=bill_count)
-objs = json.loads(response.text)
+response = []
+try:
+    for sk in range(1, requests.get('http://api.ly.g0v.tw/v0/collections/bills/').json()['paging']['count'], 100):
+        response.append(requests.get('http://api.ly.g0v.tw/v0/collections/bills/', params={'l': '100', 'sk': str(sk)}).json()['entries'])
+    objs = json.loads({'entries': response})
+except Exception, e:
+    print sk
+    raise
 dump_data = json.dumps(objs)
 write_file(dump_data, 'lyapi_bills.json')
 dump_data = json.dumps(objs, sort_keys=True, indent=4, ensure_ascii=False)
 write_file(dump_data, 'lyapi_bills(pretty_format).json')
 
-bill_count = {'l': requests.get('http://api.ly.g0v.tw/v0/collections/ttsmotions/').json()['paging']['count']}
-response = requests.get('http://api.ly.g0v.tw/v0/collections/ttsmotions/', params=bill_count)
-objs = json.loads(response.text)
+response = []
+try:
+    for sk in range(1, requests.get('http://api.ly.g0v.tw/v0/collections/ttsmotions/').json()['paging']['count'], 100):
+        response.append(requests.get('http://api.ly.g0v.tw/v0/collections/ttsmotions/', params={'l': '100', 'sk': str(sk)}).json()['entries'])
+    objs = json.loads({'entries': response})
+except Exception, e:
+    print sk
+    raise
 dump_data = json.dumps(objs)
 write_file(dump_data, 'lyapi_ttsmotions.json')
 dump_data = json.dumps(objs, sort_keys=True, indent=4, ensure_ascii=False)
