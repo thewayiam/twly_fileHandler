a969b0ba2f777e867635dd2af09c5fa62c2f82a9
thewayiam <twly.tw@gmail.com>
2014-04-10 20:55:42 +0800

    issue #5: name, columns done

--- legislator/property/property.py
+++ legislator/property/property.py
@@ -15,6 +15,14 @@ import numpy as np
 import ly_common
 
 
+def get_type(df):
+    rows = df[df.columns[0]]
+    for row in rows:
+        if pd.notnull(row) and isinstance(row, basestring):
+            match = re.search(u'(?:公職人員)?(\S*財產申報表)', row)
+            if match:
+                return match.group(1)
+
 def get_date(df):
     for column in [1, 0]:
         rows = df[df.columns[column]]
@@ -30,7 +38,8 @@ def get_name(df):
     for row in rows:
         if pd.notnull(row) and isinstance(row, basestring):
             if re.search(u'申報人姓名', row):
-                return df[df.columns[1]][rowcount]
+                potential_list = [df[df.columns[1]][rowcount], df[df.columns[2]][rowcount]]
+                return [name for name in potential_list if pd.notnull(name)][0]
         rowcount += 1
     for row in reversed(df[df.columns[0]]):
         if pd.notnull(row) and isinstance(row, basestring):
@@ -46,25 +55,30 @@ def get_table_range(rows, targets):
                 if re.search(target, rows[i]):
                     bookmarks.append({"name": target, "position": i})
                     break
+        #print '%s not found!' % target
     return sorted(bookmarks, key=itemgetter('position'))
 
 files = [f for f in glob.glob('data/*.xlsx')]
-categories = [u'土地', u'建物', u'船舶', u'汽車', u'航空器', u'現金', u'存款', u'股票', u'債券', u'基金受益憑證', u'其他有價證券', u'其他具有相當價值之財產', u'保險', u'債權', u'債務', u'事業投資', u'備註', u'備往']
+categories = [u'土地', u'建物', u'船舶', u'汽車', u'航空器', u'現金', u'存款', u'股票', u'債券', u'基金受益憑證', u'其他有價證券', u'具有相當價值之財產', u'保險', u'債權', u'債務', u'事業投資', u'備註', u'此致']
 for f in files:
+    print f
     df_orgi = pd.read_excel(f, 0, header=None, encoding='utf-8')
     df_orgi.replace(to_replace=u'監察院公報\S*', regex=True, value=nan, inplace=True)
     first_column = df_orgi[df_orgi.columns[0]]
     bookmarks = get_table_range(first_column, categories)
+    title = get_type(df_orgi)
     date = get_date(df_orgi)
     name = get_name(df_orgi)
-    writer = pd.ExcelWriter('output/%s-%s-%s-%s.xlsx' % (os.path.splitext(os.path.basename(f))[0], name, date, df_orgi[0][0]), engine='xlsxwriter')
+    writer = pd.ExcelWriter('output/%s_%s_%s_%s.xlsx' % (os.path.splitext(os.path.basename(f))[0], name, date, title), engine='xlsxwriter')
     for i in range(0, len(bookmarks) - 1):
-        df = df_orgi[bookmarks[i]['position']:bookmarks[i+1]['position']]
-        df.dropna(inplace=True, axis=1, how='all', thresh=2)
-        df = df_orgi[bookmarks[i]['position']:bookmarks[i+1]['position']].dropna(how='any', subset=[0, 1])
+        df = df_orgi[bookmarks[i]['position'] + 1 : bookmarks[i+1]['position']]
+        df.dropna(inplace=True, how='any', subset=[0, 1]) # Drop if column 0 or 1 empty
+        df.dropna(inplace=True, how='all', axis=1) # At least one item in one column
+        print df.columns
         if not df.empty:
             df.columns = map(lambda x: x.replace(' ', '') if isinstance(x, basestring) else x, df.iloc[0].replace(nan, ''))
-        df.to_excel(writer, sheet_name=bookmarks[i]['name'])
+            df[1:].to_excel(writer, sheet_name=bookmarks[i]['name'])
+            print df[1:].to_json(orient='records')
     writer.save()
 #output_pretty_file = codecs.open('./data(pretty_format)/merged.json', 'w', encoding='utf-8')
 #dump_data = json.dumps(npl_dict_list, sort_keys=True, indent=4, ensure_ascii=False)
