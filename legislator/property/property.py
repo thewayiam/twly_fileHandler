1989e011a856c68edad2daafb40a736df6db984c
thewayiam <twly.tw@gmail.com>
2014-04-14 23:40:59 +0800

    issue #5: stock modelize

--- legislator/property/property.py
+++ legislator/property/property.py
@@ -19,7 +19,7 @@ def get_type(df):
     rows = df[df.columns[0]]
     for row in rows:
         if pd.notnull(row) and isinstance(row, basestring):
-            match = re.search(u'(?:公職人員)?(\S*財產申報表)', row)
+            match = re.search(u'(?:公職人員)?(\S*(申報|通知)表)', row)
             if match:
                 return match.group(1)
 
@@ -59,7 +59,15 @@ def get_table_range(rows, targets):
     return sorted(bookmarks, key=itemgetter('position'))
 
 files = [f for f in glob.glob('data/*.xlsx')]
-categories = [u'土地', u'建物', u'船舶', u'汽車', u'航空器', u'現金', u'存款', u'股票', u'債券', u'基金受益憑證', u'其他有價證券', u'具有相當價值之財產', u'保險', u'債權', u'債務', u'事業投資', u'備註', u'此致']
+categories = [u'土地', u'建物', u'船舶', u'汽車', u'航空器', u'現金', u'存款', u'有價證券', u'股票', u'債券', u'基金受益憑證', u'其他有價證券', u'具有相當價值之財產', u'保險', u'債權', u'債務', u'事業投資', u'備註', u'此致']
+models = {
+    u"股票": {
+        "columns": ['name', 'owner', 'quantity', 'face_value', 'currency', 'total']
+    },
+    u"有價證券": {
+        "columns": ['name', 'owner', 'quantity', 'face_value', 'currency', 'total']
+    }
+}
 for f in files:
     print f
     df_orgi = pd.read_excel(f, 0, header=None, encoding='utf-8')
@@ -69,19 +77,29 @@ for f in files:
     title = get_type(df_orgi)
     date = get_date(df_orgi)
     name = get_name(df_orgi)
-    writer = pd.ExcelWriter('output/%s_%s_%s_%s.xlsx' % (os.path.splitext(os.path.basename(f))[0], name, date, title), engine='xlsxwriter')
-    for i in range(0, len(bookmarks) - 1):
-        df = df_orgi[bookmarks[i]['position'] + 1 : bookmarks[i+1]['position']]
-        df.dropna(inplace=True, how='any', subset=[0, 1]) # Drop if column 0 or 1 empty
-        df.dropna(inplace=True, how='all', axis=1) # At least one item in one column
-        if not df.empty:
-            df[1:] = df[ df[0] != df.iloc[0][0] ]
-            df.dropna(inplace=True, how='any', subset=[0, 1]) # Drop if column 0 or 1 empty
-            df.columns = map(lambda x: x.replace(' ', '') if isinstance(x, basestring) else x, df.iloc[0].replace(nan, ''))
-            df[1:].to_excel(writer, sheet_name=bookmarks[i]['name'])
-            #print df[1:].to_json(orient='records')
-    writer.save()
-#output_pretty_file = codecs.open('./data(pretty_format)/merged.json', 'w', encoding='utf-8')
-#dump_data = json.dumps(npl_dict_list, sort_keys=True, indent=4, ensure_ascii=False)
-#output_pretty_file.write(dump_data)
-#output_pretty_file.close()
+    if title == u'財產申報表':
+        writer = pd.ExcelWriter('output/normal/%s_%s_%s_%s.xlsx' % (name, date, title, os.path.splitext(os.path.basename(f))[0]), engine='xlsxwriter')
+        for i in range(0, len(bookmarks) - 1):
+            df = df_orgi[bookmarks[i]['position'] + 1 : bookmarks[i+1]['position']]
+            df.dropna(inplace=True, how='any', subset=[0, 1]) # Drop row if column 0 or 1 empty
+            df.dropna(inplace=True, how='all', axis=1, thresh=2) # Drop column if non-nan cell not more than one
+            if not df.empty:
+                df[1:] = df[ df[0] != df.iloc[0][0] ]
+                df.dropna(inplace=True, how='any', subset=[0, 1]) # Drop if column 0 or 1 empty
+                if bookmarks[i]['name'].strip() == u"股票" or bookmarks[i]['name'].strip() == u"有價證券":
+                    df.columns = models[u"股票"]["columns"]
+                else:
+                    df.columns = map(lambda x: x.replace(' ', '') if isinstance(x, basestring) else x, df.iloc[0].replace(nan, ''))
+                df[1:].to_excel(writer, sheet_name=bookmarks[i]['name'])
+                #print df[1:].to_json(orient='records')
+                #output_pretty_file = codecs.open('./data(pretty_format)/merged.json', 'w', encoding='utf-8')
+                #dump_data = json.dumps(npl_dict_list, sort_keys=True, indent=4, ensure_ascii=False)
+                #output_pretty_file.write(dump_data)
+                #output_pretty_file.close()
+        writer.save()
+    elif title == u'變動財產申報表':
+        writer = pd.ExcelWriter('output/change/%s_%s_%s_%s.xlsx' % (name, date, title, os.path.splitext(os.path.basename(f))[0]), engine='xlsxwriter')
+    elif title == u'信託財產申報表':
+        writer = pd.ExcelWriter('output/trust/%s_%s_%s_%s.xlsx' % (name, date, title, os.path.splitext(os.path.basename(f))[0]), engine='xlsxwriter')
+    else:
+        writer = pd.ExcelWriter('output/others/%s_%s_%s_%s.xlsx' % (name, date, title, os.path.splitext(os.path.basename(f))[0]), engine='xlsxwriter')
