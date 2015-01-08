6b95a0aa92d58c467c2985875c93a6a679df5bd7
thewayiam <twly.tw@gmail.com>
2015-01-09 00:17:17 +0800

    bill_param of legislator prerender

--- bill/parser.py
+++ bill/parser.py
@@ -21,8 +21,8 @@ def BillExist(bill_ref):
 
 def Bill(bill):
     c.execute('''
-        INSERT into bill_bill(uid, api_bill_id, abstract, summary, bill_type, doc, proposed_by, sitting_introduced)
-        SELECT %(bill_ref)s, %(bill_id)s, %(abstract)s, %(summary)s, %(bill_type)s, %(doc)s, %(proposed_by)s, %(sitting_introduced)s
+        INSERT into bill_bill(uid, api_bill_id, ad, abstract, summary, bill_type, doc, proposed_by, sitting_introduced)
+        SELECT %(bill_ref)s, %(bill_id)s, %(ad)s, %(abstract)s, %(summary)s, %(bill_type)s, %(doc)s, %(proposed_by)s, %(sitting_introduced)s
         WHERE NOT EXISTS (SELECT 1 FROM bill_bill WHERE uid = %(bill_ref)s)
     ''', bill)
 
@@ -49,11 +49,13 @@ def ttsMotions(motion):
 conn = db_ly.con()
 c = conn.cursor()
 
+ad = 8
 dict_list = json.load(open('lyapi_bills.json'))
 for bill in dict_list['entries']:
     if not bill['bill_ref'] or not re.search(u'L', bill['bill_ref']):
         continue
     print bill['bill_ref']
+    bill['ad'] = ad
     Bill(bill)
     for motion in bill['motions']:
         sitting_dict = {"uid": motion['sitting_id'], "ad": int(motion['sitting_id'].split('-')[0]), "session": int(motion['sitting_id'].split('-')[1][:2]), "date": motion['dates'][0]['date']}
@@ -65,10 +67,11 @@ for bill in dict_list['entries']:
     for proposal_type in ['sponsors', 'cosponsors']:
         if bill.get(proposal_type):
             for legislator in bill[proposal_type]:
-                legislator_id = ly_common.GetLegislatorId(c, legislator)
-                if legislator_id:
-                    legislator_id = ly_common.GetLegislatorDetailId(c, legislator_id, 8)
-                    LegislatorBill(legislator_id, bill['bill_ref'], priproposer, petition)
+                uid = ly_common.GetLegislatorId(c, legislator)
+                if uid:
+                    legislator_id = ly_common.GetLegislatorDetailId(c, uid, ad)
+                    if legislator_id:
+                        LegislatorBill(legislator_id, bill['bill_ref'], priproposer, petition)
                 priproposer = False
             petition = True
 conn.commit()
@@ -108,4 +111,25 @@ c.executemany('''
 ''', response)
 conn.commit()
 print 'last action information of bill done'
+
+c.execute('''
+    SELECT
+        legislator_id,
+        COUNT(*) total,
+        SUM(CASE WHEN priproposer=True THEN 1 ELSE 0 END) chief,
+        SUM(CASE WHEN priproposer=False AND petition=False THEN 1 ELSE 0 END) proposal,
+        SUM(CASE WHEN petition=True THEN 1 ELSE 0 END) petition
+    FROM bill_legislator_bill
+    GROUP BY legislator_id
+''')
+response = c.fetchall()
+for r in response:
+    param = dict(zip(['total', 'chief', 'proposal', 'petition'], r[1:]))
+    c.execute('''
+        UPDATE legislator_legislatordetail
+        SET bill_param = %s
+        WHERE id = %s
+    ''', (param, r[0]))
+conn.commit()
+print 'Update Bill param of People'
 print 'Succeed'
