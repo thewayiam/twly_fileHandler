4aafa6d99649ba4c1cc1a01b16f528f008733ec6
thewayiam <twly.tw@gmail.com>
2015-06-16 05:47:42 +0800

    include json file

--- bill/crawler.py
+++ bill/crawler.py
@@ -15,13 +15,9 @@ response = requests.get('http://api.ly.g0v.tw/v0/collections/bills/', params=bil
 objs = json.loads(response.text)
 dump_data = json.dumps(objs)
 write_file(dump_data, 'lyapi_bills.json')
-dump_data = json.dumps(objs, sort_keys=True, indent=4, ensure_ascii=False)
-write_file(dump_data, 'lyapi_bills(pretty_format).json')
 
 bill_count = {'l': requests.get('http://api.ly.g0v.tw/v0/collections/ttsmotions/').json()['paging']['count']}
 response = requests.get('http://api.ly.g0v.tw/v0/collections/ttsmotions/', params=bill_count)
 objs = json.loads(response.text)
 dump_data = json.dumps(objs)
 write_file(dump_data, 'lyapi_ttsmotions.json')
-dump_data = json.dumps(objs, sort_keys=True, indent=4, ensure_ascii=False)
-write_file(dump_data, 'lyapi_ttsmotions(pretty_format).json')
