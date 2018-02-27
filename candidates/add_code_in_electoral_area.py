89480cb8f4093b260da46d2cb45174d76458754f
thewayiam <twly.tw@gmail.com>
2018-02-28 00:45:18 +0800

    add new village from ris.gov.tw

--- candidates/add_code_in_electoral_area.py
+++ candidates/add_code_in_electoral_area.py
@@ -8,21 +8,18 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 def rename_dict_key(d):
-    columns = {
-        u"縣市代碼": "county_code",
-        u"縣市名稱": "county_name",
-        u"區里代碼": "town_code",
-        u"區鄉鎮名稱": "town_name",
-        u"村里代碼": "village_code",
-        u"村里名稱": "village_name",
-        u"村里代碼": "village_code"
+    return {
+        "county_code": d[0].strip(),
+        "county_name": d[1].strip(),
+        "town_code": d[2].strip(),
+        "town_name": d[3].strip(),
+        "village_code": d[6].strip(),
+        "village_name": d[5].strip(),
+        "new_village": d[7].strip()
     }
-    for verbose_key, key in columns.items():
-        d[key] = d.pop(verbose_key)
-    return d
 
 # get_or_create village code maps
-file_path = 'data/candidates/village_code_2016.json'
+file_path = 'data/candidates/village_code_2018.json'
 if os.path.isfile(file_path):
     maps = json.load(open(file_path))
 else:
@@ -33,17 +30,22 @@ else:
     worksheets = sh.worksheets()
     for wks in worksheets:
         maps = {}
-        rows = wks.get_all_records(head=4)
+        rows = wks.get_all_values()[4:]
         for row in rows:
             row = rename_dict_key(row)
-            maps['%s_%s_%s' % (re.sub(u'台', u'臺', row['county_name']), row['town_name'], row['village_name'], )] = {
-                'county_code': row['county_code'],
-                'town_code': row['town_code'],
-                'village_code': row['village_code']
-            }
-    with codecs.open('data/candidates/village_code_2016.json', 'w', encoding='utf-8') as outfile:
+            if not row['new_village']:
+                maps['%s_%s_%s' % (re.sub(u'台', u'臺', row['county_name']), row['town_name'], row['village_name'], )] = {
+                    'county_code': row['county_code'],
+                    'town_code': row['town_code'],
+                    'village_code': row['village_code']
+                }
+            else:
+                maps['_'.join(re.search(u'(.+?[縣市])(.+?[鄉鎮市區])(.+)', row['new_village']).groups())] = {
+                    'town_code': row['village_code'][:7],
+                    'village_code': row['village_code']
+                }
+    with codecs.open('data/candidates/village_code_2018.json', 'w', encoding='utf-8') as outfile:
         outfile.write(json.dumps(maps, indent=2, ensure_ascii=False))
-
 
 target = json.load(open('data/candidates/election_region_2016.json'))
 target_out = target.copy()
@@ -60,4 +62,3 @@ for county, v in target.items():
             region['district'][district] = villages_code
 with codecs.open('data/candidates/election_region_with_village_code_2016.json', 'w', encoding='utf-8') as outfile:
     outfile.write(json.dumps(target, indent=2, ensure_ascii=False))
-
